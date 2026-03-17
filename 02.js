2477a8b918b857641680adc7a42ab864c20570ba
pooja mishra <poojagudiya851@gmail.com>
2026-03-17 21:06:54 +0530

    Add files via upload

---
+++ 02.js
@@ -0,0 +1,39 @@
+// data type
+// primitve:- immutable datatype
+// STRING
+// let name = "pooja";
+// let greeting = 'namste';
+// let template = "User; ${name}";
+// console.log(name);
+
+
+// NUMBER
+// let integervalue = 21;
+// let floatvalue = 3.14;
+// let notanumber = NaN;
+// console.log(integervalue); 
+
+// BOOLEAN
+// let isactive = true;
+// let iscompelete = false;
+// console.log(isactive);
+
+// UNDEFINED
+// let user;
+// console.log(user);
+
+// NULL
+// let date = null;
+// console.log(null);
+
+// BIGINT
+
+// const verylargenum = 1234567890n;
+// const anotherbigint = BigInt(1234567890);
+
+// console.log(anotherbigint);
+
+// SYMBOL
+// const id1 = Symbol('id');
+// const id2 = Symbol('id');
+// console.log(id1===id2);
