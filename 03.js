503cd03b8da0edbbb30ce24763f9c8b4ef0833aa
pooja mishra <poojagudiya851@gmail.com>
2026-03-19 21:35:16 +0530

    Add files via upload

---
+++ 03.js
@@ -0,0 +1,18 @@
+// non-primitive data types
+// OBJECTS
+// let student={
+//     name: "pooja",
+//     course: "bca",
+//     age: 21
+// };
+// console.log(student)
+
+// ARRAYS
+// let number =[10,20,30,40];
+// console.log(number)
+
+// FUNCTION
+function greet(){
+    console.log("hello");
+}
+greet();
---
+++ 04.js
@@ -0,0 +1,37 @@
+// OPERATOR
+// 1)ASSIGNMENT OPERATOR
+// let a=1;
+// console.log(a);
+
+
+// 2)ARITHMATIC OP
+// let a = 20;
+// let b = 36;
+// console.log(a+b);
+
+// 3)comparison operator:- >,<,==,===
+// let a = 5;
+// let b = "5";
+
+
+// // console.log(a == b);   
+
+// // using ===
+// console.log(a === b); 
+
+// 4)LOGICAL OP
+// Logical AND (&&)
+// let a = 10;
+// let b = 20;
+// console.log(a > 5 && b > 15);  
+// // Logical OR (||)
+// let x = 5;
+// let y = 2;
+// // console.log(x > 10 || y < 5);  
+// // Logical NOT (!)
+// let isLoggedIn = false;
+// // console.log(!isLoggedIn); 
+
+
+
+
