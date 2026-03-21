3d7305935e8fe3a8111f56a5cbb9b64b6f1b9796
pooja mishra <poojagudiya851@gmail.com>
2026-03-21 13:05:44 +0530

    Add files via upload

---
+++ 05.js
@@ -0,0 +1,33 @@
+// LOOPS
+// 1)FOR LOOP
+//  for(let i=0;i<10;i++){
+//      console.log(i)
+//  }
+
+// 2) WHILE LOOP
+// let i = 0;
+// while(i < 10){
+//     console.log(i);
+//     i++;
+// }
+// 3) DO WHILE
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// }while(i<10);
+
+
+// IF-ELSE
+let age = 25;
+if (age>=25){
+console.log("can vote");
+}else if(age<25){
+    console.log("can not vote");
+
+}
+    
+
+
+
+
---
+++ 06.js
@@ -0,0 +1,7 @@
+// NUMBERS
+// let a = 10;
+// let b = 345.890;
+// let c = b.toFixed(1);
+// console.log(typeof c);
+// console.log(b.toPrecision(4));
+// console.log(b.toString(2));
