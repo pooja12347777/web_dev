823a541ea7e129c10f255a344c1157d79fa2f7ce
pooja mishra <poojagudiya851@gmail.com>
2026-03-26 19:42:54 +0530

    Add files via upload

---
+++ 09.js
@@ -0,0 +1,27 @@
+// ARRAY
+// let data = [1,2,"pooja" , true , null];
+// console.log(data);
+
+let fruits = ["apple","cherry","banana"];
+    // console.log(fruits.length);
+    // console.log(fruits[0]);
+    // GETTING THE LAST ELEMENT
+    // console.log(fruits[fruits.legth-1]);
+    // CHANGING THE ELEMENT
+    // fruits[1] = "blueberry";
+    // console.log(fruits);
+    // ADDING ELEMENTS
+    //  fruits.push("grapes");
+    // console.log(fruits);
+    // REMOVING THE ADDED ELEMENT(GRAPES)
+    // 
+    
+    // .UNSHIT;- ADD ITMES TO THE BEGNING
+    // let queue = ["a","b","c"];
+    // queue.unshift("e");
+    // console.log(queue);
+// let firstline = queue.shift();
+// console.log(firstline);
+// console.log(queue);
+
+
